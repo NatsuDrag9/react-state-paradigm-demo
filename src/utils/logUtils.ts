acb09524a418e6bb3bc70c3f6a2de6f76c6f5e8f
NatsuDrag9 <rohitimandi9@zoho.com>
2025-10-21 13:27:07 +0530

    chore(app): modify logs and remove commercialized code

--- src/utils/logUtils.ts
+++ src/utils/logUtils.ts
@@ -1,78 +1,24 @@
-import { LogArgsType, LogMessageLevel } from '@definitions/logUtilsTypes';
 import { isDevMode } from './envUtils';
-import Logger, { prodLogMessage } from './logHelperUtils';
 
-/**
- * Logs messages in development mode with a specific calling location.
- * This function is used for standard log messages.
- *
- * @param args - The messages or objects to log.
- */
-export const logInDev = (...args: LogArgsType): void => {
+// Define a type for the log function parameters to allow any type of arguments
+type LogArgs = unknown[];
+
+export const logInDev = (...args: LogArgs): void => {
   if (isDevMode()) {
-    Logger.info('LOGGING IN DEV', args);
+    console.log(
+      '%c∴ LOGGING IN DEV',
+      'font-family: monospace; font-size: 12px; color: green;'
+    );
+    console.log(...args);
   }
 };
 
-/**
- * Logs messages in development mode with full stack trace
- * This function is used detailed log messages with trace.
- *
- * @param args - The messages or objects to log.
- */
-export const vLogInDev = (...args: LogArgsType): void => {
+export const logErrorInDev = (...args: LogArgs): void => {
   if (isDevMode()) {
-    Logger.debug('VERBOSE LOGGING IN DEV', args, true);
+    console.log(
+      '%c∴ LOGGING ERROR IN DEV',
+      'font-family: monospace; font-size: 12px; color: red;'
+    );
+    console.error(...args);
   }
-};
-
-/**
- * Logs errors in development mode with a specific calling location.
- * This function is used for error logging.
- *
- * @param args - The error messages or objects to log.
- */
-export const logErrorInDev = (...args: LogArgsType): void => {
-  if (isDevMode()) {
-    Logger.error('LOGGING ERROR IN DEV', args);
-  }
-};
-
-/**
- * Logs error messages in development mode with full stack trace
- * This function is used detailed error log messages with trace.
- *
- * @param args - The messages or objects to log.
- */
-export const vLogErrorInDev = (...args: LogArgsType): void => {
-  if (isDevMode()) {
-    Logger.error('VERBOSE LOGGING ERROR IN DEV', args, true);
-  }
-};
-
-/**
- * Logs messages in dev and production mode with a specific calling location.
- * This function is used for standard log messages.
- *
- * @param args - The messages or objects to log.
- */
-export const logMessage = (...args: LogArgsType): void => {
-  Logger.info('LOGGING IN DEV', args);
-  prodLogMessage('info' as LogMessageLevel, 'LOGGING IN PROD', args, true);
-};
-
-/**
- * Logs error messages in development and production mode with full stack trace
- * This function is used detailed error log messages with trace.
- *
- * @param args - The messages or objects to log.
- */
-export const logError = (...args: LogArgsType): void => {
-  Logger.error('LOGGING ERROR IN DEV', args, true);
-  prodLogMessage(
-    'error' as LogMessageLevel,
-    'LOGGING ERROR IN PROD',
-    args,
-    true
-  );
 };
