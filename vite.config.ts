d19466d20a7023b997fb709cba18f53886a8fd5e
NatsuDrag9 <rohitimandi9@zoho.com>
2025-10-29 15:50:17 +0530

    build(app): remove analyzer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { analyzer } from 'vite-bundle-analyzer';
+// import { analyzer } from 'vite-bundle-analyzer';
 
 // https://vitejs.dev/config/
 const viteConfig = defineConfig({
-  plugins: [react(), analyzer()],
+  plugins: [react()],
   base: '/react-state-paradigm-demo/',
   resolve: {
     alias: {
